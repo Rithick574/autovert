1a3b100d787d6ac9739c2bffb3d7f03ce9792620
Rithick <rithick.panoor574@gmail.com>
2024-11-13 20:07:42 +0530

    feat:user schema created

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,24 +1,23 @@
-import { Schema,model } from "mongoose";
+import { Schema, model } from "mongoose";
 import bcrypt from "bcryptjs";
 import { IUser } from "@/types/user.type";
 
-const userSchema = new Schema<IUser>({
-  email: { type: String, required: true, unique: true },
-  password: {
-    type: String,
-    required: true,
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    isActive: { type: Boolean, default: true },
+    lastPasswordChanged: { type: Date, required: true, default: Date.now },
+    version: { type: Number, ref: "Versions" },
   },
-  role: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["admin", "user"],
-      message: "Role must be either 'admin' or 'user'",
-    },
-  },
-});
+  { timestamps: true }
+);
 
-userSchema.methods.matchPassword = async function (password: string): Promise<boolean> {
+userSchema.methods.matchPassword = async function (
+  password: string
+): Promise<boolean> {
   console.log("password", password, this.password, this.email);
   return await bcrypt.compare(password, this.password);
 };
@@ -31,12 +30,11 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   try {
     const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt); 
+    this.password = await bcrypt.hash(this.password, salt);
     next();
-  } catch (error:any) {
+  } catch (error: any) {
     next(error);
   }
 });
-
 
 export const userModel = model<IUser>("Users", userSchema);
--- server/src/types/user.type.ts
+++ server/src/types/user.type.ts
@@ -1,8 +1,15 @@
 import { Document } from "mongoose";
 
 export interface IUser extends Document {
+  name:string;
   email: string;
   password: string;
   role: "admin" | "user";
+  lastPasswordChanged:Date;
+  version:number;
+  isActive:true;
+  createdAt:Date;
+  updatedAt:Date;
   matchPassword(password: string): Promise<boolean>;
+  checkIsadmin(email: String): Promise<any>;
 }
